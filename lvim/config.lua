9aef5af6c35d1a5352049805b24c9ea5564ae600
Meliq Pilosyan <melopilosyan@hey.com>
2024-01-02 00:26:08 +0400

    lvim: Changes loading of the iamcco/markdown-preview.nvim plugin on `cmd = MarkdownPreviewToggle`

--- lvim/config.lua
+++ lvim/config.lua
@@ -335,8 +335,7 @@ lvim.plugins = {
 
   {
     "iamcco/markdown-preview.nvim",
-    ft = { "markdown" },
-    cmd = { "MarkdownPreview" },
+    cmd = { "MarkdownPreviewToggle" },
     build = function() vim.fn["mkdp#util#install"]() end,
     config = function() vim.g.mkdp_filetypes = { "markdown" } end,
   },
