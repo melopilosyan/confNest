2c94dfcebf0aa89ecc12f4f898a61a08be2e8d46
Meliq Pilosyan <melopilosyan@hey.com>
2024-02-10 12:23:37 +0400

    Adds the <leader>t. mapping, invoking rspec-integrated "repeat last run"

--- lvim/config.lua
+++ lvim/config.lua
@@ -185,6 +185,8 @@ with(lvim.builtin.which_key.mappings, function(lm)
       "<cmd>lua require('rspec.integrated').run_spec_file{only_current_example = true}<cr>",
       "RSpec run current example"
     },
+    ["."] = { "<cmd>lua require('rspec.integrated').run_spec_file{repeat_last_run = true}<cr>",
+      "RSpec repeat last run" },
     w = { "<cmd>lua require('mp.rspec.floating_window').run()<cr>", "RSpec run in window" },
     p = { "<cmd>TSPlaygroundToggle<cr>", "Treesitter playground toggle" },
     m = { "<cmd>MarkdownPreviewToggle<cr>", "Markdown preview toggle" },
