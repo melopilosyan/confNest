ffbceadf7040086aaf1fe4c8ea9936eb86cc6ec6
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-15 03:15:21 +0400

    Updates the main installer
    Calls `apt autoremove`.
    Sets the final idle and lock settings and bash `-e` options.

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 # Be fancy
 source ~/.local/share/omakub/ascii.sh
 
@@ -12,12 +15,13 @@ gsettings set org.gnome.desktop.session idle-delay 0
 # Run installers
 for script in ~/.local/share/omakub/install/*.sh; do source $script; done
 
-# Upgrade everything that might ask for a reboot last
+# Upgrade everything that might ask for a reboot
 sudo apt upgrade -y
+sudo apt autoremove -y
 
-# Revert to normal idle and lock settings
-gsettings set org.gnome.desktop.screensaver lock-enabled false
-gsettings set org.gnome.desktop.session idle-delay 300
+# Set the final idle and lock settings
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 480
 
 # Logout to pickup changes
 gum confirm "Ready to logout for all settings to take effect?" && gnome-session-quit --logout --no-prompt
