9dff4a4402c6588481d583bd75acdd74936e2e26
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-15 22:09:49 +0400

    Adds kitty installer & copies settings from Projects/configs
    Installs the default theme during app installation via `+kitten themes`.
    
    Config files relation/structure:
    Kitty's default `kitty.conf` includes the `omakub/configs/kitty/kitty.conf`,
    which in turn includes partials from `omakub/configs/kitty`. So all changes
    inside omakub will be visible to kitty. In addition the default `kitty.conf`
    can contain workspace specific settings that don't need to be committed.
    Like the `touch_scroll_multiplier`.
    
    Themes are managed by kitty itself through `~/.config/kitty/theme.conf`.

---
+++ install/app-kitty.sh
@@ -0,0 +1,18 @@
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/bin/
+
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=$HOME/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=$HOME/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+
+gsettings set org.gnome.desktop.default-applications.terminal exec 'kitty'
+
+mkdir -p ~/.config/kitty
+
+cat <<CONF > ~/.config/kitty/kitty.conf
+include ~/.local/share/omakub/configs/kitty/kitty.conf
+touch_scroll_multiplier 5
+CONF
+
+# Set the default theme
+kitty +kitten themes --config-file-name=theme.conf 'Tokyo Night Storm'
