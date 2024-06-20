00e6285733cdd6a538862b0e71075a8bbdadd00d
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-21 03:50:14 +0400

    Simplifies the font selector
    Removes Meslo and Fira fonts and setting the default in
    install/fonts.sh. JetBrains is the default in Kitty config.
    Removes the NERD_FONT variable and inlines the fonts/_set.sh partial.
    Installs Cascadia font via install:cascadia_code rake task from
    \configs.

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,34 +1,6 @@
-mkdir -p ~/.local/share/fonts
-
-cd ~/Downloads
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
-unzip CascadiaMono.zip -d CascadiaFont
-cp CascadiaFont/*.ttf ~/.local/share/fonts
-rm -rf CascadiaMono.zip CascadiaFont
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraMono.zip
-unzip FiraMono.zip -d FiraMono
-cp FiraMono/*.otf ~/.local/share/fonts
-rm -rf FiraMono.zip FiraMono
-
 cd ~/Projects/configs
 rake install:jetbrains_mono
+rake install:cascadia_code
 cd -
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip
-unzip Meslo.zip -d Meslo
-cp Meslo/*.ttf ~/.local/share/fonts
-rm -rf Meslo.zip Meslo
-
-wget https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
-unzip master.zip -d iaFonts
-cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
-rm -rf master.zip iaFonts
-
-fc-cache
-cd -
-
-# List available font names: `kitty +list-fonts`.
-
-# Set JetBrains Mono as default.
-FONT=jetbrains-mono sourrce $OMAKUB_PATH/fonts/_set.sh
+fc-cache -f
