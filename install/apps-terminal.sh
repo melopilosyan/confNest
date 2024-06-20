90a795da931bca7820446f453f19778c700ea4af
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-20 21:22:18 +0400

    Adds fortune to the terminal apps list

--- install/apps-terminal.sh
+++ install/apps-terminal.sh
@@ -1,1 +1,3 @@
-sudo apt install -y fzf ripgrep fd-find bat eza zoxide plocate btop apache2-utils xclip
+sudo apt install -y \
+  fzf ripgrep fd-find bat eza zoxide plocate btop apache2-utils xclip \
+  fortune
