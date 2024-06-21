bedaa64f51122b620985f1f45be2b2235331dce5
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-21 23:26:10 +0400

    Install rustup's minimal profile

--- install/2-rustup.sh
+++ install/2-rustup.sh
@@ -1,3 +1,3 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --profile="minimal"
 
 . ~/.cargo/env
