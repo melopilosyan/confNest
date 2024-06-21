e3b95b8c36f973ef1afaddbad80fd39412b5fe8d
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-21 18:32:28 +0400

    Installs Ruby latest after RVM installation

--- install/rvm.sh
+++ install/rvm.sh
@@ -1,3 +1,6 @@
 gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
 \curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
+
+RUBY_LATEST=$(curl -s "https://api.github.com/repos/ruby/ruby/releases/latest" | sed -nr 's/.*"name": "v(.*)",/\1/p')
+rvm install $RUBY_LATEST -C "--enable-yjit"
