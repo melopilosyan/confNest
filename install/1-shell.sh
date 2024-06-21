4581075c65f798b6dc2ab09f60ba48961d466da3
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-21 19:30:47 +0400

    Makes gext from extentions installer available on initial installation

--- install/1-shell.sh
+++ install/1-shell.sh
@@ -1,5 +1,9 @@
 # NOTE: Find the OS default .bashrc at /etc/skel/.bashrc
 
+# Extentions installer requires ~/.local/bin in the PATH.
+# Make it available diring the initial installation.
+if [[ $PATH == *~/.local/bin* ]]; then PATH="$HOME/.local/bin:$PATH"; fi
+
 if ! grep -wq OMAKUB_PATH ~/.bashrc; then
   echo -e "\nexport OMAKUB_PATH='$OMAKUB_PATH'" >> ~/.bashrc
   echo 'source $OMAKUB_PATH/configs/shell/bashrc' >> ~/.bashrc
