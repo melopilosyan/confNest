cc1360874c67b4bea3824c769f1c6dc089d6f1ee
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-24 03:30:56 +0400

    Changes the configs repo origin URL schema to git

--- install/1-configs.sh
+++ install/1-configs.sh
@@ -7,4 +7,9 @@ mkdir -p ~/.local/bin
 echo -e "\nexport CONFIGS_DIR='$CONFIGS_DIR'" >> ~/.bashrc
 
 echo "Cloning configs into $CONFIGS_DIR ..."
-git clone https://github.com/melopilosyan/configs.git $CONFIGS_DIR > /dev/null
+git clone -q https://github.com/melopilosyan/configs.git $CONFIGS_DIR
+
+cd $CONFIGS_DIR
+git remote set-url origin git@github.com:melopilosyan/configs.git
+cd -
+
