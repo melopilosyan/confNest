27b36801d7c14fd138dd191ce14f4939172664ab
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-24 03:34:32 +0400

    Moves creating the ~/.local/bin into 1-shell.sh - feels more appropriate

--- install/1-configs.sh
+++ install/1-configs.sh
@@ -2,8 +2,6 @@ export CONFIGS_DIR=${CONFIGS_DIR:-$HOME/Projects/configs}
 
 [ -d $CONFIGS_DIR ] && return
 
-mkdir -p ~/.local/bin
-
 echo -e "\nexport CONFIGS_DIR='$CONFIGS_DIR'" >> ~/.bashrc
 
 echo "Cloning configs into $CONFIGS_DIR ..."
--- install/1-shell.sh
+++ install/1-shell.sh
@@ -3,6 +3,7 @@
 # Extentions installer requires ~/.local/bin in the PATH.
 # Make it available diring the initial installation.
 if [[ $PATH != *~/.local/bin* ]]; then PATH="$HOME/.local/bin:$PATH"; fi
+mkdir -p ~/.local/bin
 
 if ! grep -wq OMAKUB_PATH ~/.bashrc; then
   echo -e "\nexport OMAKUB_PATH='$OMAKUB_PATH'" >> ~/.bashrc
