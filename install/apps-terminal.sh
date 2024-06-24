60ce88a0f4d5aa989904725e6be6c295c42ebc03
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-24 09:05:22 +0400

    Changes installation of the fd tool fetching the latest from github

--- install/apps-terminal.sh
+++ install/apps-terminal.sh
@@ -1,3 +1,9 @@
 sudo apt install -y \
-  fzf ripgrep fd-find bat eza zoxide plocate btop apache2-utils xclip \
+  fzf ripgrep bat eza zoxide plocate btop apache2-utils xclip \
   vim fortune
+
+# Install latest fd realease from github
+FD_LATEST=$(curl -s "https://api.github.com/repos/sharkdp/fd/tags?per_page=1" | sed -nr 's/.*"name": "v(.*)",/\1/p')
+curl -sLo fd.deb "https://github.com/sharkdp/fd/releases/download/v$FD_LATEST/fd-musl_${FD_LATEST}_amd64.deb"
+sudo apt install -y ./fd.deb
+rm fd.deb
