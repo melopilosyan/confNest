47275a8317c71be0af6505c54738b0e7905b0417
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-24 04:09:30 +0400

    Adds check for existence of the OMAKUB_PATH in ~/.bashrc before sourcing the bashrc

--- install/1-shell.sh
+++ install/1-shell.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.local/bin
 
 if ! grep -wq OMAKUB_PATH ~/.bashrc; then
   echo -e "\nexport OMAKUB_PATH='$OMAKUB_PATH'" >> ~/.bashrc
-  echo 'source $OMAKUB_PATH/configs/shell/bashrc' >> ~/.bashrc
+  echo '[ -d $OMAKUB_PATH ] && source $OMAKUB_PATH/configs/shell/bashrc' >> ~/.bashrc
 fi
 
 [ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
