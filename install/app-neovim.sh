49eecb633a7cdfcbcd16fd8562471abc53816bb8
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-24 07:26:38 +0400

    Comment out LazyVim installation for now

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -5,7 +5,7 @@ rake install:neovim
 bin/setup
 cd -
 
-if [ ! -d "$HOME/.config/nvim" ]; then
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
-	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
-fi
+#if [ ! -d "$HOME/.config/nvim" ]; then
+#	git clone https://github.com/LazyVim/starter ~/.config/nvim
+#	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
+#fi
