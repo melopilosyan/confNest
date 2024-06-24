0ebdb141ac929a6c9fe20d40e1e868c89a223849
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-24 08:47:27 +0400

    Skip RVN/rustup installation if Ruby/Rust is installed

--- install/2-rustup.sh
+++ install/2-rustup.sh
@@ -1,3 +1,5 @@
+command -v rustc >/dev/null && echo "Rust is installed. Skipping rustup installation." && return
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --profile="minimal"
 
 . ~/.cargo/env
