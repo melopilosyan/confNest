12bf74bd08994ecf30522ea642457ac4d5320573
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-30 01:16:55 +0400

    Removes quotes escaping from resulting ~/.gitconfig

--- install/set-git.sh
+++ install/set-git.sh
@@ -3,18 +3,18 @@ git config --global user.email melopilosyan@hey.com
 
 git config --global alias.aa 'add -A'
 git config --global alias.b branch
-git config --global alias.changed 'show --pretty="format:" --name-only'
+git config --global alias.changed "show --pretty='format:' --name-only"
 git config --global alias.cm commit
 git config --global alias.cmamend 'commit --amend'
 git config --global alias.cmm 'log --format=%B -n 1'
 git config --global alias.co checkout
 git config --global alias.df diff
 git config --global alias.lastcc 'show --name-only'
-git config --global alias.ls 'log --pretty=format:"%C(yellow)%h\ %C(green)%ad%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --date=short --graph'
+git config --global alias.ls "log --pretty=format:'%C(yellow)%h %C(green)%ad%Cred%d %Creset%s%Cblue [%cn]' --decorate --date=short --graph"
 git config --global alias.nb 'checkout -b'
 git config --global alias.set-remote-origin 'remote set-url origin'
 git config --global alias.st status
-git config --global alias.zup 'for-each-ref --sort="authordate:iso8601" --format="%(color:green)%(authordate:relative)%09%(color:white)%(refname:short) / %(contents:subject)" refs/heads'
+git config --global alias.zup "for-each-ref --sort='authordate:iso8601' --format='%(color:green)%(authordate:relative)%09%(color:white)%(refname:short) / %(contents:subject)' refs/heads"
 
 git config --global merge.tool meld
 git config --global pull.rebase true
