1174be3168ea0524f3bbec08e2d71d20123127cf
Meliq Pilosyan <melopilosyan@hey.com>
2024-06-30 20:49:45 +0400

    Moves ulauncher configs to their dedicated first level folder
    Updates the installer by sym-linking the files instead of copying -
    makes farther changes immediate.
    And removes the initial launch in favor of manually creating the
    config folder.

--- install/ulauncher.sh
+++ install/ulauncher.sh
@@ -1,12 +1,15 @@
-sudo add-apt-repository universe -y
-sudo add-apt-repository ppa:agornostal/ulauncher -y
-sudo apt update -y
-sudo apt install -y ulauncher
+if ! command -v ulauncher >/dev/null; then
+  sudo add-apt-repository universe -y
+  sudo add-apt-repository ppa:agornostal/ulauncher -y
+  sudo apt update -y
+  sudo apt install -y ulauncher
+fi
 
-# Start ulauncher to have it populate config before we overwrite
-mkdir -p ~/.config/autostart/
-cp $OMAKUB_PATH/configs/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
-gtk-launch ulauncher.desktop >/dev/null 2>&1
-sleep 2 # ensure enough time for ulauncher to set defaults
-cp $OMAKUB_PATH/configs/ulauncher.json ~/.config/ulauncher/settings.json
-cp $OMAKUB_PATH/configs/ulauncher_shortcuts.json ~/.config/ulauncher/shortcuts.json
+mkdir -p ~/.config/autostart ~/.config/ulauncher
+
+# Install the launcher
+ln -sf "$CONFIGS_DIR/ulauncher/ulauncher.desktop" ~/.config/autostart/ulauncher.desktop
+
+# Install configs
+ln -sf "$CONFIGS_DIR/ulauncher/settings.json" ~/.config/ulauncher/settings.json
+ln -sf "$CONFIGS_DIR/ulauncher/shortcuts.json" ~/.config/ulauncher/shortcuts.json
