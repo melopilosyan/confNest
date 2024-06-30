fbf2538a8bdb5386a5ff96a525929bcd6531c412
Meliq Pilosyan <melopilosyan@hey.com>
2024-07-01 02:44:37 +0400

    Moves functions from .bash_aliases to bash/functions.sh
    Drops gcmm(not using it anyway) and prettyjson[_s](in favor of jq).

--- bash/functions.sh
+++ bash/functions.sh
@@ -5,3 +5,22 @@ g() {
     git status
   fi
 }
+
+git-search-by-term() {
+  local term=$1
+  local show_lines_around=${2:-4}
+  # shellcheck disable=SC2086
+  git log --oneline -S "$term" | awk '{ print $1 }' | xargs git show |
+    grep -C $show_lines_around "$term"
+}
+
+slvim() {
+  lvim "$1" && source "$1"
+}
+
+# Show the RSS for the process filtered by command part.
+# RSS: resident set size, the non-swapped physical memory that a task has used (in kiloBytes).
+prss() {
+  # shellcheck disable=SC2009
+  ps -e -o pid,rss,cmd | grep -E "$1|RSS" | sed '$d'
+}
