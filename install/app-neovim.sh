bedc9d4f3ba82595ff1121d7781dd49c3c6502ca
Meliq Pilosyan <melopilosyan@hey.com>
2024-07-01 21:22:54 +0400

    Moves LunarVim installtion into app-neovim.sh
    bin/setup is no longer in use, keeping for reference for now.

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -1,9 +1,14 @@
-cd $CONFIGS_DIR
-rake install:neovim
+rake -C "$CONFIGS_DIR" install:neovim
 
-# LunarVim requires installed Neovim
-bin/setup
-cd -
+test -d ~/.config/lvim && return
+
+# Link local lvim as ~/.config/lvim. $_ is the last argument of the previous command.
+ln -sf "$CONFIGS_DIR/lvim" "$_"
+
+# TODO: Migrate neovim configuration on top of [LazyVim](https://www.lazyvim.org)
+#
+# Install [LunarVim](https://www.lunarvim.org)
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh) --no-install-dependencies
 
 #if [ ! -d "$HOME/.config/nvim" ]; then
 #	git clone https://github.com/LazyVim/starter ~/.config/nvim
