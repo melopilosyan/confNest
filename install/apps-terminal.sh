f244d46a8539d7e8d0ee465c0776a7a6b8a0f222
Meliq Pilosyan <melopilosyan@hey.com>
2024-07-02 05:11:03 +0400

    Moves gum installer into apps-terminal.sh

--- install/apps-terminal.sh
+++ install/apps-terminal.sh
@@ -7,3 +7,6 @@ install_deb_package_from_gh 'sharkdp/fd' 'fd_VERSION_amd64'
 
 # rg - recursively search the current directory for lines matching a pattern
 install_deb_package_from_gh 'BurntSushi/ripgrep' 'ripgrep_VERSION-1_amd64'
+
+# gum - a tool for glamorous shell scripts
+install_deb_package_from_gh 'charmbracelet/gum' 'gum_VERSION_amd64'
