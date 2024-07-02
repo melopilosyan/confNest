d03af89a23b3c6d265cd00d6af9d13b315e4072a
Meliq Pilosyan <melopilosyan@hey.com>
2024-07-02 05:11:03 +0400

    Moves gum installer into apps-terminal.sh

--- install/app-gum.sh
+++
@@ -1,8 +0,0 @@
-# Gum is used for the Omakub commands for tailoring Omakub after the initial install
-GUM_LATEST=$(curl -s "https://api.github.com/repos/charmbracelet/gum/tags?per_page=1" | sed -nr 's/.*"name": "v(.*)",/\1/p')
-echo "Installing gum version $GUM_LATEST ..."
-
-curl -sLo gum.deb "https://github.com/charmbracelet/gum/releases/download/v$GUM_LATEST/gum_${GUM_LATEST}_amd64.deb"
-sudo apt install -y ./gum.deb
-
-rm gum.deb
