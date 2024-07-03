b646d3b3ea2974912f3d25db6b1f35e0401beaec
Meliq Pilosyan <melopilosyan@hey.com>
2024-07-04 01:16:38 +0400

    Extracts setup.sh from install/1-shell.sh
    Creates local bash completions folder.

--- install/1-shell.sh
+++ install/1-shell.sh
@@ -1,10 +1,5 @@
 # NOTE: Find the OS default .bashrc at /etc/skel/.bashrc
 
-# Extentions installer requires ~/.local/bin in the PATH.
-# Make it available diring the initial installation.
-if [[ $PATH != *~/.local/bin* ]]; then PATH="$HOME/.local/bin:$PATH"; fi
-mkdir -p ~/.local/bin
-
 if ! grep -wq CONFIGS_DIR ~/.bashrc; then
   echo -e "\nexport CONFIGS_DIR=$CONFIGS_DIR" >> ~/.bashrc
   echo '[ -d $CONFIGS_DIR ] && source $CONFIGS_DIR/bash/bashrc' >> ~/.bashrc
@@ -12,6 +7,3 @@ fi
 
 [ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
 echo "\$include $CONFIGS_DIR/readline/inputrc" > ~/.inputrc
-
-# Make functions available diring initial installation.
-source "$CONFIGS_DIR/bash/functions.sh"
