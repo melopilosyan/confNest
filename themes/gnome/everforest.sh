9213f71fa91c7d3a0346775ac26c12e686af825f
Meliq Pilosyan <melopilosyan@hey.com>
2024-08-01 19:31:39 +0400

    Refactors gnome theme changer into a single _set.sh

--- themes/gnome/everforest.sh
+++
@@ -1,3 +0,0 @@
-OMAKUB_THEME_COLOR="bark"
-OMAKUB_THEME_BACKGROUND="fog_forest_2.jpg"
-source "$CONFIGS_DIR/themes/gnome/_set-gnome-theme.sh"
--- themes/gnome/nord.sh
+++
@@ -1,3 +0,0 @@
-OMAKUB_THEME_COLOR="blue"
-OMAKUB_THEME_BACKGROUND="nord_scenary.png"
-source "$CONFIGS_DIR/themes/gnome/_set-gnome-theme.sh"
