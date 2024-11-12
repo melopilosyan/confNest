60a49f57003cf0f4d763b011c8b0f7e343ec3b61
Meliq Pilosyan <melopilosyan@hey.com>
2024-11-13 03:54:17 +0400

    bash: Refactors and optimizes bashmarks.sh for Bash
    Renames g to j - g is for git here.
    Renames s to b, adding optional second parameter to specify the
    absolute path of the bookmark.
    Handles errors with proper return codes and prints messages to
    standard error.

--- bash/rc.d/bashmarks.sh
+++ bash/rc.d/bashmarks.sh
@@ -21,151 +21,130 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# Copied from https://github.com/huyng/bashmarks
+# Initially copied from https://github.com/huyng/bashmarks
 # at https://github.com/huyng/bashmarks/blob/ef37b3749313cd41a742826ffc9a2381a4637bfa/bashmarks.sh
+#
+# Copyright (c) 2024 Meliq Pilosyan
+# Published under the MIT License (https://opensource.org/license/mit)
 
-# USAGE:
-# s bookmarkname - saves the curr dir as bookmarkname
-# g bookmarkname - jumps to the that bookmark
-# g b[TAB] - tab completion is available
-# p bookmarkname - prints the bookmark
-# p b[TAB] - tab completion is available
-# d bookmarkname - deletes the bookmark
-# d [TAB] - tab completion is available
-# l - list all bookmarks
+# Create the file to store bookmarks
+export SDIRS="${SDIRS:-$HOME/.sdirs}"
+test -f "$SDIRS" || touch "$SDIRS"
 
-# setup file to store bookmarks
-if [ ! -n "$SDIRS" ]; then
-    SDIRS=~/.sdirs
-fi
-touch "$SDIRS"
+# Remove alias "l" from default .bashrc
+alias l &>/dev/null && unalias l >/dev/null
 
-RED="0;31m"
-GREEN="0;33m"
+_print_help_message() {
+  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo 'Bookmark frequently used directories
 
-# save current directory to bookmarks
-function s {
-    check_help $1
-    _bookmark_name_valid "$@"
-    if [ -z "$exit_message" ]; then
-        _purge_line "$SDIRS" "export DIR_$1="
-        CURDIR=$(echo $PWD| sed "s#^$HOME#\$HOME#g")
-        echo "export DIR_$1=\"$CURDIR\"" >> $SDIRS
-    fi
+Usage:
+  b <name> [ABSOLUTE_PATH] - Bookmark current or ABSOLUTE_PATH directory as "name"
+  j <name>                 - Jump/cd to the directory bookmarked as "name"
+  p <name>                 - Print the directory path for "name"
+  d <name>                 - Delete the bookmark
+  l                        - List all bookmarks
+
+Tab autocimplition is available for b, d and p.'
+    return 0
+  fi
+  return 1
 }
 
-# jump to bookmark
-function g {
-    check_help $1
-    source $SDIRS
-    target="$(eval $(echo echo $(echo \$DIR_$1)))"
-    if [ -d "$target" ]; then
-        cd "$target"
-    elif [ ! -n "$target" ]; then
-        echo -e "\033[${RED}WARNING: '${1}' bashmark does not exist\033[00m"
-    else
-        echo -e "\033[${RED}WARNING: '${target}' does not exist\033[00m"
-    fi
+_valid_bookmark_name() {
+  if [ -z "$1" ]; then
+    >&2 echo "Bookmark name is required."
+    return 1
+  elif [ "$1" != "${1//[^A-Za-z0-9_]/}" ]; then
+    >&2 echo "Bookmark name is invalid. Use only alphanumeric characters and the '_'."
+    return 2
+  fi
 }
 
-# print bookmark
-function p {
-    check_help $1
-    source $SDIRS
-    echo "$(eval $(echo echo $(echo \$DIR_$1)))"
+_set_bookmark_path() {
+  source "$SDIRS"
+
+  local env_name="_BM_$1"
+  path="${!env_name}" # Substitute _BM_name with its value
 }
 
-# delete bookmark
-function d {
-    check_help $1
-    _bookmark_name_valid "$@"
-    if [ -z "$exit_message" ]; then
-        _purge_line "$SDIRS" "export DIR_$1="
-        unset "DIR_$1"
-    fi
+_delete_saved_bookmark() {
+  if [ -s "$SDIRS" ] && grep "$1=" "$SDIRS" >/dev/null; then
+    temp=$(mktemp -t bashmarks.XXXX) || exit 1
+    # shellcheck disable=SC2064
+    trap "/bin/rm -f -- '$temp'" EXIT
+
+    sed "/$1=/d" "$SDIRS" >"$temp"
+    /bin/mv "$temp" "$SDIRS"
+
+    /bin/rm -f -- "$temp"
+    trap - EXIT
+  fi
 }
 
-# print out help for the forgetful
-function check_help {
-    if [ "$1" = "-h" ] || [ "$1" = "-help" ] || [ "$1" = "--help" ] ; then
-        echo ''
-        echo 's <bookmark_name> - Saves the current directory as "bookmark_name"'
-        echo 'g <bookmark_name> - Goes (cd) to the directory associated with "bookmark_name"'
-        echo 'p <bookmark_name> - Prints the directory associated with "bookmark_name"'
-        echo 'd <bookmark_name> - Deletes the bookmark'
-        echo 'l                 - Lists all available bookmarks'
-        kill -SIGINT $$
-    fi
+b() {
+  _print_help_message "$1" && return 0
+  _valid_bookmark_name "$1" || return
+
+  _delete_saved_bookmark "$1"
+  echo "_BM_$1=\"${2:-$PWD}\"" >>"$SDIRS"
 }
 
-# list bookmarks with dirnam
-function l {
-    check_help $1
-    source $SDIRS
+j() {
+  _print_help_message "$1" && return 0
+  _valid_bookmark_name "$1" || return
 
-    # if color output is not working for you, comment out the line below '\033[1;32m' == "red"
-    env | sort | awk '/^DIR_.+/{split(substr($0,5),parts,"="); printf("\033[0;33m%-20s\033[0m %s\n", parts[1], parts[2]);}'
+  _set_bookmark_path "$1"
 
-    # uncomment this line if color output is not working with the line above
-    # env | grep "^DIR_" | cut -c5- | sort |grep "^.*="
+  if [ -d "$path" ]; then
+    cd "$path" || return
+  elif [ -z "$path" ]; then
+    >&2 echo "'${1}' bashmark does not exist"
+    return 3
+  else
+    >&2 echo "'${path}' is not a directory"
+    return 4
+  fi
 }
-# list bookmarks without dirname
-function _l {
-    source $SDIRS
-    env | grep "^DIR_" | cut -c5- | sort | grep "^.*=" | cut -f1 -d "="
+
+p() {
+  _print_help_message "$1" && return 0
+  _valid_bookmark_name "$1" || return
+
+  _set_bookmark_path "$1"
+  echo "$path"
 }
 
-# validate bookmark name
-function _bookmark_name_valid {
-    exit_message=""
-    if [ -z $1 ]; then
-        exit_message="bookmark name required"
-        echo $exit_message
-    elif [ "$1" != "$(echo $1 | sed 's/[^A-Za-z0-9_]//g')" ]; then
-        exit_message="bookmark name is not valid"
-        echo $exit_message
-    fi
+d() {
+  _print_help_message "$1" && return 0
+  _valid_bookmark_name "$1" || return
+
+  _delete_saved_bookmark "$1"
+  unset "_BM_$1"
+}
+
+l() {
+  source "$SDIRS"
+
+  for env_name in ${!_BM_*}; do
+    printf "\e[0;33m%-20s\e[0m %s\n" "${env_name/_BM_/}" "${!env_name}"
+  done
 }
 
 # completion command
-function _comp {
-    local curw
-    COMPREPLY=()
-    curw=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=($(compgen -W '`_l`' -- $curw))
-    return 0
+function _bookmark_comp {
+  source "$SDIRS"
+
+  local env_names="${!_BM_*}" curw=${COMP_WORDS[COMP_CWORD]}
+  # shellcheck disable=SC2207
+  COMPREPLY=($(compgen -W "${env_names//_BM_/}" -- "$curw"))
+  return 0
 }
 
-# ZSH completion command
-function _compzsh {
-    reply=($(_l))
-}
+# Enable programmable completion facilities (see Programmable Completion in bash(1)).
+shopt -s progcomp
 
-# safe delete line from sdirs
-function _purge_line {
-    if [ -s "$1" ]; then
-        # safely create a temp file
-        t=$(mktemp -t bashmarks.XXXXXX) || exit 1
-        trap "/bin/rm -f -- '$t'" EXIT
-
-        # purge line
-        sed "/$2/d" "$1" > "$t"
-        /bin/mv "$t" "$1"
-
-        # cleanup temp file
-        /bin/rm -f -- "$t"
-        trap - EXIT
-    fi
-}
-
-# bind completion command for g,p,d to _comp
-if [ $ZSH_VERSION ]; then
-    compctl -K _compzsh g
-    compctl -K _compzsh p
-    compctl -K _compzsh d
-else
-    shopt -s progcomp
-    complete -F _comp g
-    complete -F _comp p
-    complete -F _comp d
-fi
+# Setup completion for j, p, d functions/commands
+complete -F _bookmark_comp j
+complete -F _bookmark_comp p
+complete -F _bookmark_comp d
