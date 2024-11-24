d8dcdb89a55e00122128d939a40ea7f7902dddf2
Meliq Pilosyan <melopilosyan@hey.com>
2024-11-24 21:52:09 +0400

    Fixes typos in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ mkdir -p ~/.local/share/bash-completion/completions
 # Prepare local man folder
 mkdir -p ~/.local/share/man/man1
 
-# Extentions installer requires ~/.local/bin in the PATH.
+# Extensions installer requires ~/.local/bin in the PATH.
 if [[ $PATH != *~/.local/bin* ]]; then PATH="$HOME/.local/bin:$PATH"; fi
 
-# Make functions available diring initial installation.
+# Make functions available during initial installation.
 source "$CONFIGS_DIR/bash/functions.sh"
