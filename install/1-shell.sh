12532dc6db082499ae5ed3bcd9d1201294b1bbdf
Meliq Pilosyan <melopilosyan@hey.com>
2024-11-27 05:26:39 +0400

    installer: Comments out alias l in ~/.bashrc allowing bashmarks [l]ist function

--- install/1-shell.sh
+++ install/1-shell.sh
@@ -1,5 +1,7 @@
 # NOTE: Find the OS default .bashrc at /etc/skel/.bashrc
 
+sed -i 's/^alias l=/# &/' ~/.bashrc
+
 if ! grep -wq "$CONFIGS_DIR" ~/.bashrc; then
   echo -e "\nexport CONFIGS_DIR=$CONFIGS_DIR" >> ~/.bashrc
   echo 'test -s $CONFIGS_DIR/bash/bashrc && source "$_"' >> ~/.bashrc
