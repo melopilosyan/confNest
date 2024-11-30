968070122981ca3c95dce3cc60b6075bd4b1fd57
Meliq Pilosyan <melopilosyan@hey.com>
2024-11-30 18:24:53 +0400

    installer: Set fonts installer to remove previous version (on subsequent runs)
    Fixes shellcheck warnings for cd command

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,5 +1,5 @@
-cd $CONFIGS_DIR
-rake install:jetbrains_mono || echo -e "\n>>> JetBrains font installation failed. Take a look. <<<\n"
-rake install:cascadia_code || echo -e "\n>>> Cascadia font installation failed. Take a look. <<<\n"
-rake install:nerd_font || echo -e "\n>>> Nerd font installation failed. Take a look. <<<\n"
-cd -
+cd "$CONFIGS_DIR" || return
+rake install:jetbrains_mono[,t] || echo -e "\n>>> JetBrains font installation failed. Take a look. <<<\n"
+rake install:cascadia_code[,t] || echo -e "\n>>> Cascadia font installation failed. Take a look. <<<\n"
+rake install:nerd_font[,t] || echo -e "\n>>> Nerd font installation failed. Take a look. <<<\n"
+cd - || return
