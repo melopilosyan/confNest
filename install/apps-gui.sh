711f9baf0dd16c6b91bc6d809eeb423e0ddf5969
Meliq Pilosyan <melopilosyan@hey.com>
2025-02-05 06:27:09 +0400

    installer: Sets xarchiver the default opener for its supported file types

--- install/apps-gui.sh
+++ install/apps-gui.sh
@@ -1,1 +1,6 @@
 sudo apt install -y gimp xarchiver
+
+echo "Making xarchiver the default opener for its supported file types"
+xdg-mime default xarchiver.desktop "$(
+  sed -n '/^MimeType=/ { s/MimeType=//; y/;/ /; p }' /usr/share/applications/xarchiver.desktop
+)"
