add52c9a352a069fc89928edcec356203bdaaaac
Meliq Pilosyan <melopilosyan@hey.com>
2025-12-19 23:36:46 +0400

    lvim: Sets leap mappings in the right new way :)

--- lvim/config.lua
+++ lvim/config.lua
@@ -307,7 +307,10 @@ lvim.plugins = {
     "ggandor/leap.nvim",
     event = "VeryLazy",
     config = function ()
-      require('leap').add_default_mappings()
+      -- :help leap-mappings
+      vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
+      vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
+      vim.keymap.set('n',             'gs', '<Plug>(leap-from-window)')
     end,
   },
 
