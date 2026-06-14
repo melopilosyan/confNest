70b5c4e207fb50d574b668163cf1ac6bb0c7ca0d
Meliq Pilosyan <melopilosyan@hey.com>
2026-06-15 01:57:12 +0400

    lvim: Changes solargraph with ruby_lsp
    Sets the `cmd` to ruby_lsp executable per Ruby version.
    Implements the handler for the custom `rubyLsp.openFile` code lens.

--- lvim/lua/config/options.lua
+++ lvim/lua/config/options.lua
@@ -7,7 +7,7 @@ vim.g.snacks_animate = false
 
 -- LSP Server to use for Ruby.
 -- Set to "solargraph" to use solargraph instead of ruby_lsp.
-vim.g.lazyvim_ruby_lsp = "solargraph"
+vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
 
 require("mp.options")
--- lvim/lua/plugins/lsp.lua
+++ lvim/lua/plugins/lsp.lua
@@ -1,3 +1,17 @@
+-- Handlers ruby-lsp's openFile code lens commands.
+-- `command.arguments` can be:
+--    "file:///full/path/to/file.rb#L123"
+--    "file:///full/path/to/file.rb"
+vim.lsp.commands["rubyLsp.openFile"] = function(command)
+  local parts = vim.split(command.arguments[1][1] --[[@as string]], "#L")
+  local prefix = "file://" .. vim.uv.cwd()
+  local path = string.sub(parts[1], #prefix + 2)
+  local line_num = tonumber(parts[2]) or 1
+
+  vim.cmd.edit(vim.fn.fnameescape(path))
+  vim.api.nvim_win_set_cursor(0, { line_num, 0 })
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -8,6 +22,12 @@ return {
           mason = false, -- disable auto-installing with mason
         },
         ruby_lsp = {
+          mason = false, -- disable auto-installing with mason
+
+          -- https://shopify.github.io/ruby-lsp/editors.html#lazyvim-lsp
+          cmd = { vim.fn.expand(string.format("~/.rvm/rubies/%s/bin/ruby-lsp", vim.env.RUBY_VERSION)) },
+        },
+        rubocop = {
           mason = false, -- disable auto-installing with mason
         },
       },
