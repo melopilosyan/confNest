f3c5f3050e3638ac5c9adc666261a3769410ad1f
Meliq Pilosyan <melopilosyan@hey.com>
2026-07-24 01:53:59 +0400

    lvim: Adds a handy playground to collect and write data in a split window

--- lvim/lua/mp/globals.lua
+++ lvim/lua/mp/globals.lua
@@ -20,3 +20,50 @@ _G.lazy_require = function(require_path)
     end,
   })
 end
+
+local api = vim.api
+local win = -1
+local buf = -1
+
+_G.write_in_a_split_window = function(data)
+  if not api.nvim_buf_is_valid(buf) then
+    -- Create one as an unlisted "throwaway" |scratch-buffer|
+    buf = api.nvim_create_buf(false, true)
+    vim.keymap.set("n", "q", ":bwipeout<cr>", { buf = buf })
+  end
+
+  if not api.nvim_win_is_valid(win) then
+    -- Open top-level right split without number line and sign column
+    win = api.nvim_open_win(buf, true, { split = "right", win = -1 })
+    -- api.nvim_set_option_value("number", false, { win = win })
+    api.nvim_set_option_value("relativenumber", false, { win = win })
+    api.nvim_set_option_value("signcolumn", "no", { win = win })
+  end
+
+  vim.schedule(function()
+    local lines = vim.split(vim.inspect(data), "\n", { plain = true })
+    api.nvim_buf_set_lines(buf, -1, -1, false, lines)
+  end)
+end
+
+_G.playground = {
+  data = {},
+  collect = function(self, data)
+    if self.stoppped then return end
+    table.insert(self.data, data)
+  end,
+  write = function(self, data)
+    if self.stopped then return end
+    write_in_a_split_window(data)
+  end,
+  stop = function(self)
+    self.stoppped = true
+  end,
+  reset = function(self)
+    self.data = {}
+    self.stoppped = false
+  end,
+  write_data = function(self)
+    write_in_a_split_window(self.data)
+  end,
+}
